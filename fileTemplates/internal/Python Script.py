6509fefd69a92add6000b9574e6c64f91a968253
Charlie Tian <charlie.tian@spsoft-cn.com>
2016-12-15 13:32:58 +0800

    PY-2016.3.1 <charlie@localhost Create IntelliLang.xml, jQuery.xml, charlie.icls, project.default.xml, diff.xml, applicationLibraries.xml, file.template.settings.xml, ignore.xml, find.xml, #set( $MyName = "Charlie Tian" ).py, code.style.schemes.xml, git.xml, Zen HTML.xml, Vim Desert.icls, github_settings.xml, Default.xml, Python Unit Test.py, laf.xml, duplocatorSettings.xml, Default2.xml, Zen XSL.xml, Default copy.xml, editor.codeinsight.xml, editor.xml, uml.xml, hg.xml, webServers.xml, debugger.xml, Charlie bak1.xml, path.macros.xml, baseRefactoring.xml, ide.general.xml, charlie2.icls, Charlie.xml, user.xml, other.xml, Mathematica.xml, keymap.xml, code.style.schemes, ui.lnf.xml, filetypes.xml, Python Script.py, plugin_ui.xml, watcherDefaultTasks.xml, colors.scheme.xml, External Tools.xml, notifications.xml, abbreviations.xml, databaseDrivers.xml, conda_packages.xml, Conf.xml, SQL.xml, customization.xml, jdk.table.xml, Default _1_.xml, web-browsers.xml, proxy.settings.xml, tasks.xml, vcs.xml, Copy of #set( $MyName = "Charlie Tian" ).py, databaseSettings.xml, usageView.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__title__ = '$NAME'
+__author__ = 'Charlie Tian'
+__email__ = 'tianhongjie07@gmail.com'
+__mtime__ = '${YEAR}-${MONTH}-${DAY}'
+
---
+++ fileTemplates/internal/Python Unit Test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__author__ = '$USER'
+
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+if __name__ == '__main__':
+    unittest.main()
